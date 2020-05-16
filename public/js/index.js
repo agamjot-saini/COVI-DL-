52b75536b946017cacfe4af7fa8ff14590c5f5f2
Lasitha Amuwala <lasithaamuwala@gmail.com>
2020-05-16 14:17:18 -0400

    commit main.js

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-
-
-    document.getElementById("xrayButton").onclick = function () {
-        location.href = "/xray";
-    };
-
-    document.getElementById("patientButton").onclick = function () {
-        location.href = "/patient";
-    };
