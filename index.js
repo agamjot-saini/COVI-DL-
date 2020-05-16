685a99bfb180f86c384d043e942b1ec63897c2c6
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2020-05-16 14:30:59 -0400

    Update

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const fs = require('fs');
+const fs = require("fs");
+const multer = require("multer");
+
 // Imports the Google Cloud client library.
-const {Storage} = require('@google-cloud/storage');
-const {PredictionServiceClient} = require('@google-cloud/automl').v1;
+const { Storage } = require("@google-cloud/storage");
+const { PredictionServiceClient } = require("@google-cloud/automl").v1;
 
 const projectId = "boxwood-plating-277400";
 const location = "us-central1";
 const modelId = "ICN4592119109499486208";
 
-
-
 const client = new PredictionServiceClient({
-  projectId: 'boxwood-plating-277400',
-  keyFilename: path.join(__dirname, "creds.json")
+  projectId: "boxwood-plating-277400",
+  keyFilename: path.join(__dirname, "creds.json"),
 });
 
-const storage = new Storage({
-  projectId: 'boxwood-plating-277400',
-  keyFilename: path.join(__dirname, "creds.json")
-});
-
+// const storage = new Storage({
+//   projectId: "boxwood-plating-277400",
+//   keyFilename: path.join(__dirname, "creds.json"),
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,6 +30,20 @@ app.use(
     extensions: ["html"],
   })
 );
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
 async function predict(locallyStoredFileName) {
   const filePath = path.join(__dirname, locallyStoredFileName);
@@ -54,16 +67,19 @@ async function predict(locallyStoredFileName) {
     // console.log(
     //   `Predicted class score: ${annotationPayload.classification.score}`
     // );
-    return [annotationPayload.displayName, annotationPayload.classification.score];
+    return [
+      annotationPayload.displayName,
+      annotationPayload.classification.score,
+    ];
   }
 }
 
-predict("pneumonia.jpg").then(results => {
-  console.log("Class: " + results[0]);
-  console.log("Score: " + results[1]);
-}).catch(err => {
-  console.error(err);
-});
+// predict("pneumonia.jpg").then(results => {
+//   console.log("Class: " + results[0]);
+//   console.log("Score: " + results[1]);
+// }).catch(err => {
+//   console.error(err);
+// });
 
 // async function init() {
 //   try {
@@ -81,15 +97,11 @@ predict("pneumonia.jpg").then(results => {
 // init();
 
 app.get("/predictXRay", (req, res) => {
-  
-
-  
   res.send("ye ok");
-})
+});
 
 const PORT = 443;
 app.listen(process.env.PORT || PORT, (err) => {
-    if (err) console.log;
-    console.log(`Server started on port: ${PORT}`);
+  if (err) console.log;
+  console.log(`Server started on port: ${PORT}`);
 });
-
